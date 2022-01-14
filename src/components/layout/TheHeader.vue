5368f5aee07dcca7d8c10502a4b57b98244faa16
Digno Alexander Familia <dfamilia@cdevteam.com>
2022-01-14 11:21:41 -0400

    create layout structure

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <nav>
+      <h1>Welcome marti</h1>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: #3d008d;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  margin: 0;
+}
+
+h1 a {
+  color: white;
+  margin: 0;
+}
+
+h1 a:hover,
+h1 a:active,
+h1 a.router-link-active {
+  border-color: transparent;
+}
+
+header nav {
+  width: 90%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <the-header />
+  <router-view v-slot="slotProps">
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.component" />
+    </transition>
+  </router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import TheHeader from "@/components/layout/TheHeader.vue";
+
+export default defineComponent({
+  name: "MainWrapper",
+  components: { TheHeader },
+});
+</script>
+
+<style></style>
