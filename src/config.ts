0bfb87b6d21aba2a471d0aae738af16c77ebbc6b
Digno Alexander Familia <dfamilia@cdevteam.com>
2022-01-14 22:53:54 -0400

    fix names of env variables

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import { ApiModel } from '@/core/api/api.interface';
 
-const { BASE_URL, API_BASE_URL } = process.env;
+const { BASE_URL, VUE_APP_API_BASE_URL } = process.env;
 
 const API: ApiModel = {
-  BASE_URL: API_BASE_URL,
+  BASE_URL: VUE_APP_API_BASE_URL,
   TIMEOUT: 30000,
 };
 
