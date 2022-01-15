4bd2d17f2d5b3777883134c7f6f125b04d8ecdad
Digno Alexander Familia <dfamilia@cdevteam.com>
2022-01-15 00:06:49 -0400

    add customily colors to header background

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <nav class="navbar navbar-dark bg-primary">
+    <nav class="navbar navbar-dark customily-bg-theme">
       <div class="container-fluid">
         <span class="navbar-brand mb-0 h1">Welcome marti</span>
       </div>
@@ -8,4 +8,16 @@
   </header>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.navbar {
+  &.customily-bg-theme {
+    --bs-bg-opacity: 1;
+    background: rgb(119, 98, 226);
+    background: linear-gradient(
+      90deg,
+      rgba(119, 98, 226, 1) 0%,
+      rgba(138, 195, 210, 1) 100%
+    );
+  }
+}
+</style>
