98e7b5bcf3a370d3ab0e7640013751e7dcb4d257
Digno Alexander Familia <dfamilia@cdevteam.com>
2022-01-15 15:22:41 -0400

    implement route guard to check the token

--- src/router/events.ts
+++ src/router/events.ts
@@ -1,9 +1,14 @@
+import { useAuthStore } from '@/modules/auth/store';
 import { Router } from 'vue-router';
 
 export const applyGuards = (router: Router): void => {
   router.beforeEach(async (to, _, next) => {
+    const authStore = useAuthStore();
     // TO DO: verify if the user is authenticated
     // if not refresh token
+    if (!authStore.isAuthenticated) {
+      await authStore.login();
+    }
     return next();
   });
 };
