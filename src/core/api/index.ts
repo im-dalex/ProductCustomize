535918f8558377bc66d67e329672ec1d8cb90a6c
Digno Alexander Familia <dfamilia@cdevteam.com>
2022-01-15 15:21:59 -0400

    implement axios interceptor to attach the token to the request

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -1,5 +1,6 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { API } from '@/config';
+import { useAuthStore } from '@/modules/auth/store';
 
 export const api = axios.create({
   baseURL: API.BASE_URL,
@@ -9,3 +10,18 @@ export const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+api.interceptors.request.use(
+  (res: AxiosRequestConfig) => {
+    const { headers } = res;
+    const authStore = useAuthStore();
+
+    if (headers) {
+      headers[
+        'Authorization'
+      ] = `${authStore.token.token_type} ${authStore.token.access_token}`;
+    }
+    return res;
+  },
+  (err) => err
+);
