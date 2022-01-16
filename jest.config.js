d14bc7505fd2baeebdd06aca4822ba0a114a7f79
Digno Alexander Familia <dfamilia@cdevteam.com>
2022-01-16 03:51:48 -0400

    set jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   transform: {
+    '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.vue$': 'vue-jest',
   },
+  moduleFileExtensions: ['ts', 'vue'],
+  snapshotSerializers: ['jest-serializer-vue'],
+  collectCoverage: false,
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
 };
