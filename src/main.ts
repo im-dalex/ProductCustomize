d8a38657900b3d98ea0276e186ec0c9b9199a665
Digno Alexander Familia <dfamilia@cdevteam.com>
2022-01-17 10:31:49 -0400

    install vue media query library

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import store from './store';
 
 // Plugins
 import '@/plugins/customily.setup';
+import { Vue3Mq } from 'vue3-mq';
+import '@/plugins/injections.setup';
 
 // Libraries
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,4 +16,4 @@ import 'bootstrap';
 // App styles
 import '@/assets/scss/styles.scss';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).use(Vue3Mq).mount('#app');
---
+++ src/plugins/injections.setup.ts
@@ -0,0 +1,8 @@
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    mq: any;
+  }
+}
+
+export {};
