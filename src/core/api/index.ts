7320e9695ab6035f5baef10f021e29da4d44eb32
Digno Alexander Familia <dfamilia@cdevteam.com>
2022-01-17 11:12:32 -0400

    set axios interceptor to change the app loading state

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { API } from '@/config';
 import { useAuthStore } from '@/modules/auth/store';
+import { useAppStore } from '@/modules/app/store';
 
 export const api = axios.create({
   baseURL: API.BASE_URL,
@@ -10,6 +11,9 @@ export const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+// commonly these interceptor functions
+// should be in separate files
 
 api.interceptors.request.use(
   (res: AxiosRequestConfig) => {
@@ -21,7 +25,28 @@ api.interceptors.request.use(
         'Authorization'
       ] = `${authStore.token.token_type} ${authStore.token.access_token}`;
     }
+
+    // start loading state
+    useAppStore().isLoading = true;
+
     return res;
   },
-  (err) => err
+  (err) => {
+    // stop loading state
+    useAppStore().isLoading = false;
+    return err;
+  }
+);
+
+api.interceptors.response.use(
+  (res) => {
+    // stop loading state
+    useAppStore().isLoading = false;
+    return res;
+  },
+  (err) => {
+    // stop loading state
+    useAppStore().isLoading = false;
+    return err;
+  }
 );
