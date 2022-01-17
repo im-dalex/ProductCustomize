65bf216d1ff5799ac58439cd5d9bda641611e8eb
Digno Alexander Familia <dfamilia@cdevteam.com>
2022-01-17 11:10:56 -0400

    implement vue-loading-overlay

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,20 +1,31 @@
 <template>
   <the-header />
   <main class="page-content">
-    <router-view v-slot="slotProps">
-      <transition name="route" mode="out-in">
-        <component :is="slotProps.Component" />
-      </transition>
-    </router-view>
+    <loading
+      v-model:active="isLoading"
+      loader="bars"
+      color="#7762e2"
+      :width="90"
+      :height="90"
+    />
+    <router-view />
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/runtime-core';
+import { defineComponent } from 'vue';
 import TheHeader from '@/components/layout/TheHeader.vue';
+import { mapState } from 'pinia';
+import { useAppStore } from '@/modules/app/store';
+
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 export default defineComponent({
   name: 'MainWrapper',
-  components: { TheHeader },
+  components: { TheHeader, Loading },
+  computed: {
+    ...mapState(useAppStore, ['isLoading']),
+  },
 });
 </script>
