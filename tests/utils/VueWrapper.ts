06dd146af676696b24fd7082ab3772499817a4c5
Digno Alexander Familia <dfamilia@cdevteam.com>
2022-01-16 23:00:39 -0400

    create wrapper type for tests

---
+++ tests/utils/VueWrapper.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { VueWrapper } from '@vue/test-utils/dist/vueWrapper';
+
+type Wrapper = (options?: any) => VueWrapper<any>;
+
+export { Wrapper };
